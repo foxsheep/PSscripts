a39e76ff758822691ba8da219a6f19c5e4e7dd6a
foxsheep <foxsheeph@gmail.com>
2017-04-04 14:28:58 +0800

    My first ps1: ping_test
    useful for wifi random disconnecting test.

---
+++ ping_test/ping_test.ps1
@@ -0,0 +1,46 @@
+﻿#ping test. default destination is gateway.
+
+param
+(
+    $des = (Get-NetIPConfiguration).IPv4DefaultGateway[0].NextHop,
+    $t1 = 10,
+    $t2 = 100,
+    $p = 1000
+)
+
+$t1Cnt = $t2Cnt = $toutCnt = $pingCnt = 0
+$StartTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss:ff"
+
+Write-Host "`n//------------------------------------------------------//`n"
+Write-Host "Pinging $des, started at $StartTime. `n"
+
+while(1)
+{
+    $rt = (Test-Connection $des -Count 1).ResponseTime
+    $pingCnt = $pingCnt + 1
+
+    if($rt -le $t1){$t1Cnt = $t1Cnt + 1}
+    elseif($rt -le $t2){$t2Cnt = $t2Cnt + 1}
+    else
+    {
+        $toutCnt = $toutCnt + 1
+        Write-Host "`r                                                                                                           " -NoNewline
+        Write-Host "`r$toutCnt timeout at $($StartTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss:ff")"
+    }
+
+    $lost = ($toutCnt/$pingCnt * 100).ToString("F2")
+
+    Write-Host "`r$pingCnt " -ForegroundColor white -NoNewline
+    Write-Host "pings:  " -NoNewline
+    Write-Host "$t1Cnt " -ForegroundColor green -NoNewline
+    Write-Host "<$t1 ms; " -NoNewline
+    Write-Host "$t2Cnt " -ForegroundColor yellow -NoNewline
+    Write-Host "<$t2 ms; " -NoNewline
+    Write-Host "$toutCnt " -ForegroundColor red -NoNewline
+    Write-Host ">$t2 ms. " -NoNewline
+    Write-Host "$lost% " -ForegroundColor red -NoNewline
+    Write-Host "lost. " -NoNewline
+
+    Start-Sleep -Milliseconds $p
+}
+
